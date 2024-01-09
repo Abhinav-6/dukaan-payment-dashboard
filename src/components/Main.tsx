4fa707ed94f7d2745212188e2bf65ddfc70a9882
Abhinav <abhinavkumar34002@gmail.com>
2024-01-09 23:44:01 +0530

    extra ui finished

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,6 +6,16 @@ export default function Main() {
     <div className="ml-64 bg-[rgba(255,255,255,0.95)]">
       <Header />
       <Overview />
+      <div className="pb-6 pt-8 px-8 flex flex-col gap-5">
+        <h4 className="font-medium text-2xl text-[#1A181E] m-0 p-0">
+          Transactions | This Month
+        </h4>
+        <div className="flex items-center gap-2">
+          <button className="rounded-2xl px-2 py-1 bg-gray-300 opacity-80">Payouts(22)</button>
+          <button className="rounded-2xl px-2 py-1 bg-[#146EB4] text-white">Refunds(6)</button>
+        </div>
+      </div>
+      
     </div>
   );
 }
